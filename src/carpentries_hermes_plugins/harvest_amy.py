b7697d089a824feaf17835ae7957b1ce34491ad9
Toby Hodges <tbyhdgs@gmail.com>
2023-09-28 13:43:43 +0200

    outline what harvest plugin should do

---
+++ src/carpentries_hermes_plugins/harvest_amy.py
@@ -0,0 +1,8 @@
+'''
+the steps look something like this:
+* read list of contributors obtained from the other sources
+* authenticate to AMY
+* retreive orcid, opt-in preferences for each contributor
+* add these to existing Person object in the metadata
+* extend the existing metadatacontext to include this new info
+'''
