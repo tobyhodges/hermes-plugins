90418003fe81437330a9e0f421558a25fb303f90
Toby Hodges <tbyhdgs@gmail.com>
2023-09-28 13:48:58 +0200

    add note about what (not?) to do with process_amy

---
+++ src/carpentries_hermes_plugins/process_amy.py
@@ -0,0 +1,8 @@
+'''
+Based on conversation with Michael,
+we probably will not need to write a plugin to remove people
+who have opted out:
+instead, he plans to add functionality to the core of HERMES
+that will allow the removal of objects in the metadata based on
+some condition being met.
+'''
