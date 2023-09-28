55a787ec56d0634d18e08c4495212644f9eca355
Toby Hodges <tbyhdgs@gmail.com>
2023-09-28 10:41:10 +0200

    correct key names; configure logging

--- src/carpentries_hermes_plugins/deposit_trainingmaterial_jsonld.py
+++ src/carpentries_hermes_plugins/deposit_trainingmaterial_jsonld.py
@@ -11,6 +11,8 @@ from hermes.model.context import CodeMetaContext
 from hermes import config
 from hermes.error import MisconfigurationError
 
+_log = logging.getLogger('hermes.deposit.trainingmaterial_jsonld')
+
 def export(click_ctx: click.Context, ctx: CodeMetaContext):
     '''Write out a JSON-LD file of TrainingMaterial metadata.
 
@@ -20,15 +22,15 @@ def export(click_ctx: click.Context, ctx: CodeMetaContext):
     trainingmaterial_jsonld_config = config.get("deposit").get("trainingmaterial_jsonld", {})
 
     # pull out the name of the output file that was set in the config file
-    output_json = invenio_config.get("output")
+    output_json = trainingmaterial_jsonld_config.get("output")
     if output_json is None:
         raise MisconfigurationError("deposit.trainingmaterial_jsonld.output is not configured")
 
-    logging.info(f'depositing JSON-LD output in {output_json}.')
+    _log.info(f'depositing JSON-LD output in {output_json}.')
 
     # get some fields from the harvested metadata
-    title = ctx['title']
-    license = ctx['license']
+    title = ctx['codemeta.name']
+    license = ctx['codemeta.license']
 
     # create a JSON object from the extracted fields
     json_blob = json.dumps({'title': title,
